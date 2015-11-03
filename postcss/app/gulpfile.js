e2a8df59a506aad14e9f2ee88a697379f1257d77
Steve Blaurock <sblaurock@gmail.com>
2015-11-03 09:21:42 -0500

    Add precss task to Gulp. Update notes.

--- postcss/app/gulpfile.js
+++ postcss/app/gulpfile.js
@@ -2,8 +2,15 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
 
-// Process CSS
-gulp.task('css', function () {
+// Watch for changes
+gulp.task('watch', function () {
+    return gulp.watch([
+        'assets/source.css'
+    ], ['css']);
+});
+
+// Process CSS - postcss-simple-vars
+//gulp.task('css', function () {
   //var postcss = require('gulp-postcss');
 
   //return gulp.src('assets/source.css')
@@ -14,12 +21,17 @@ gulp.task('css', function () {
     //.pipe(gulp.dest('assets'));
 //});
 
-// Watch for changes
-gulp.task('watch', function () {
-    return gulp.watch([
-        'assets/source.css'
-    ], ['css']);
-});
+// Process CSS - precss
+//gulp.task('css', function () {
+  //var postcss = require('gulp-postcss');
+
+  //return gulp.src('assets/source.css')
+    //.pipe(postcss([
+      //require('precss')
+    //]))
+    //.pipe(rename('compiled.css'))
+    //.pipe(gulp.dest('assets'));
+//});
 
 // Default
 gulp.task('default', ['css', 'watch']);
