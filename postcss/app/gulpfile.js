30bd8c5dc46073eec834bd8d336aba4084a5c5f1
Steve Blaurock <sblaurock@gmail.com>
2015-11-03 08:17:16 -0500

    Define Gulp behavior. Remove excess markup from Bootstrap demonstration file.

--- postcss/app/gulpfile.js
+++ postcss/app/gulpfile.js
@@ -1,5 +1,25 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
+var watch = require('gulp-watch');
+
+// Process CSS
+gulp.task('css', function () {
+  //var postcss = require('gulp-postcss');
+
+  //return gulp.src('assets/source.css')
+    //.pipe(postcss([
+      //require('postcss-simple-vars')
+    //]))
+    //.pipe(rename('compiled.css'))
+    //.pipe(gulp.dest('assets'));
+//});
+
+// Watch for changes
+gulp.task('watch', function () {
+    return gulp.watch([
+        'assets/source.css'
+    ], ['css']);
+});
 
 // Default
-gulp.task('default', function(callback) {
-});
+gulp.task('default', ['css', 'watch']);
